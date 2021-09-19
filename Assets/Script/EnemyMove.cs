72c2eb67c5e748b8902b4cf7411f7fdba2cb4dc5
gamegame2 <59419591+dks2312@users.noreply.github.com>
2021-09-20 07:14:24 +0900

    2D 플랫포머 - 몬스터 AI 구현하기 [유니티 기초 강좌 B18]

---
+++ Assets/Script/EnemyMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer; 
+    public int nextMove;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 2);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.2f, rigid.position.y);
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if(rayHit.collider == null){
+           Turn();
+        }
+    }
+
+    void Think(){
+        nextMove = Random.Range(-1, 2);
+
+        anim.SetInteger("walkSpeed", nextMove);
+        if(nextMove != 0){
+           spriteRenderer.flipX = nextMove == 1;
+        }
+        
+        float thinkMoveTime = Random.Range(2f, 5f);
+        Invoke("Think", thinkMoveTime);   
+    }
+
+    void Turn(){
+        nextMove = nextMove * -1; 
+        spriteRenderer.flipX = nextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 2);
+    }
+}
--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
@@ -17,6 +18,11 @@ public class PlayerMove : MonoBehaviour
     }
 
     void Update(){
+        if(Input.GetButtonDown("Jump") && !anim.GetBool("isJumping")){
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
         if(Input.GetButtonUp("Horizontal")){
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
@@ -44,5 +50,17 @@ public class PlayerMove : MonoBehaviour
         } else if(rigid.velocity.x < maxSpeed*(-1)){
             rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
         }
+
+        if(rigid.velocity.y < 0){
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if(rayHit.collider != null){
+                if(rayHit.distance < 0.5f)
+                    anim.SetBool("isJumping", false);
+            }
+        }
+        
     }
 }
